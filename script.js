2a26d6449e3fffd39f2f6c6b091e3e1736ae1978
Fabio Penna <ffmpenna@gmail.com>
2022-07-13 21:15:38 -0300

    correção da junção do código

--- script.js
+++ script.js
@@ -1,18 +1,13 @@
 function verifyLogin() {
-  const email = document.getElementsByTagName('input')[0].value;
-  const pass = document.getElementsByTagName('input')[1].value;
-  if (email === 'tryber@teste.com' && pass === '123456') {
-    alert('Olá, Tryber!');
+  const email = document.getElementsByTagName("input")[0].value;
+  const pass = document.getElementsByTagName("input")[1].value;
+  if (email === "tryber@teste.com" && pass === "123456") {
+    alert("Olá, Tryber!");
   } else {
-    alert('Email ou senha inválidos.');
+    alert("Email ou senha inválidos.");
   }
 }
 
-document.getElementsByTagName('button')[0].addEventListener('click', verifyLogin);
-
-function countChars(obj) {
-  const counter = document.querySelector('#counter');
-  counter.innerHTML = 500 - obj.value.length;
-  console.log(500 - obj.value.length);
-}
-countChars();
+document
+  .getElementsByTagName("button")[0]
+  .addEventListener("click", verifyLogin);

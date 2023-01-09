ee9f406cfb335eb699a4bf7cf9dd3e69a30bc54a
Fabio Penna <ffmpenna@gmail.com>
2023-01-09 15:29:14 -0300

    feat: implementação da biblioteca Bootstrap

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
 function verifyLogin() {
-  const email = document.getElementsByTagName('input')[0].value;
-  const pass = document.getElementsByTagName('input')[1].value;
-  if (email === 'tryber@teste.com' && pass === '123456') {
-    alert('Olá, Tryber!');
+  const email = document.getElementsByTagName("input")[0].value;
+  const pass = document.getElementsByTagName("input")[1].value;
+  if (email === "tryber@teste.com" && pass === "123456") {
+    alert("Olá, Tryber!");
   } else {
-    alert('Email ou senha inválidos.');
+    alert("Email ou senha inválidos.");
   }
 }
 
 document
-  .getElementsByTagName('button')[0]
-  .addEventListener('click', verifyLogin);
+  .getElementsByTagName("button")[0]
+  .addEventListener("click", verifyLogin);
 
 function countChars() {
-  const obj = document.getElementById('textarea');
+  const obj = document.getElementById("textarea");
   const maxChar = 500;
-  const counter = document.querySelector('#counter');
+  const counter = document.querySelector("#counter");
   counter.innerHTML = maxChar - obj.value.length;
   console.log(maxChar - obj.value.length);
 }
 
-document.getElementById('textarea').addEventListener('keyup', countChars);
+document.getElementById("textarea").addEventListener("keyup", countChars);
 
 let count = 0;
 function agreement() {
   count += 1;
-  const submitBtn = document.getElementById('submit-btn');
+  const submitBtn = document.getElementById("submit-btn");
   if (count % 2 !== 0) {
     submitBtn.disabled = false;
   } else {
@@ -33,7 +33,7 @@ function agreement() {
   }
 }
 
-document.getElementById('agreement').addEventListener('click', agreement);
+document.getElementById("agreement").addEventListener("click", agreement);
 
 function getSubjects() {
   const subjects = document.querySelectorAll('input[class="subject"]:checked');
@@ -45,16 +45,16 @@ function getSubjects() {
 }
 
 function getValues() {
-  const name = document.querySelector('#input-name').value;
-  const lastName = document.querySelector('#input-lastname').value;
+  const name = document.querySelector("#input-name").value;
+  const lastName = document.querySelector("#input-lastname").value;
   const values = [
     `${name} ${lastName}`,
-    document.querySelector('#input-email').value,
-    document.querySelector('#house').value,
+    document.querySelector("#input-email").value,
+    document.querySelector("#house").value,
     document.querySelector('input[name="family"]:checked').value,
     getSubjects(),
     document.querySelector('input[name="rate"]:checked').value,
-    document.getElementById('textarea').value,
+    document.getElementById("textarea").value,
   ];
 
   return values;
@@ -62,16 +62,16 @@ function getValues() {
 
 function writeNewPage(array, form) {
   const formResult = [
-    'Nome: ',
-    'Email: ',
-    'Casa: ',
-    'Família: ',
-    'Matérias: ',
-    'Avaliação: ',
-    'Observações: ',
+    "Nome: ",
+    "Email: ",
+    "Casa: ",
+    "Família: ",
+    "Matérias: ",
+    "Avaliação: ",
+    "Observações: ",
   ];
   for (let index = 0; index < array.length; index += 1) {
-    const createParagraph = document.createElement('p');
+    const createParagraph = document.createElement("p");
     form.appendChild(createParagraph);
     createParagraph.innerText = formResult[index] + array[index];
   }
@@ -80,14 +80,15 @@ function writeNewPage(array, form) {
 function updates(event) {
   event.preventDefault();
   const inputsForm = getValues();
-  const oldForm = document.querySelector('#evaluation-form');
-  const main = document.querySelector('main');
-  const newForm = document.createElement('form');
-  newForm.setAttribute('id', 'form-data');
+  const oldForm = document.querySelector("#evaluation-form");
+  const main = document.querySelector("main");
+  const newForm = document.createElement("div");
+  newForm.setAttribute("id", "form-data");
+  newForm.setAttribute("class", "col");
 
   main.prepend(newForm);
-  oldForm.style.display = 'none';
+  oldForm.style.display = "none";
   writeNewPage(inputsForm, newForm);
 }
 
-document.getElementById('submit-btn').addEventListener('click', updates);
+document.getElementById("submit-btn").addEventListener("click", updates);
